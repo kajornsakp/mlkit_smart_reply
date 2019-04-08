000c1554505a733d8a12e9519c14c799f4f2b0eb
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2019-04-08 11:37:04 +0700

    Initial Commit

---
+++ firebaseNLP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseNLP
+//
+//  Created by Kajornsak Peerapathananont on 8/4/2562 BE.
+//  Copyright © 2562 Kajornsak Peerapathananont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
